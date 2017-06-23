17adcc1ad07f1fe8b226b0d8b0b1676cf21cb51e
Royce <royce.remulla@amaysim.com.au>
2017-06-23 14:25:59 +0800

    Updated with some string functions

---
+++ strings/basic.py
@@ -0,0 +1,9 @@
+# Split string
+text = 'Hello World'
+print(text.split(' ')[0])  # => Hello
+
+# replace
+print(text.replace('World', 'Earth'))  # => Hello
+
+# replace
+print('Hello   \n'.rstrip())  # => Hello
